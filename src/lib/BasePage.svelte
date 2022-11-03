6b8b49bfd9804e4f7afc21a9aa4cb6441a094216
joelq-oci <joelq@objectcomputing.com>
2022-11-03 15:08:27 -0600

    added TerrainLayer object to map

--- src/lib/BasePage.svelte
+++ src/lib/BasePage.svelte
@@ -1,26 +1,8 @@
 <script lang="ts">
+    
     import MapView from "./MapView.svelte";
-    import MapViewDeckGL from "./MapViewDeckGL.svelte";
 
-    let showWebGL: boolean = true;
-
-    function handleWebGlClick() {
-        showWebGL = true;
-    }
-
-    function handleDeckGlClick() {
-        showWebGL = false;
-    }
 </script>
 
-<button on:click={handleWebGlClick}>See WebGL Map</button>
-
-<button on:click={handleDeckGlClick}>See DeckGL Map</button>
-
-{#if showWebGL}
-<h2>WebGL Map</h2>
+<h2>Terrain Map</h2>
 <MapView />
-{:else}
-<h2>DeckGL Map</h2>
-<MapViewDeckGL />
-{/if}
